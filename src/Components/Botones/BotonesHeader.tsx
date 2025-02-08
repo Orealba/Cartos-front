48718a1662475a376bc0c267675e65d1fdb039ec
Orealba Soriano <orealbas@gmail.com>
2025-02-08 22:48:59 +0100

    buttons ready with responsive in header

--- src/Components/Botones/BotonesHeader.tsx
+++ src/Components/Botones/BotonesHeader.tsx
@@ -1,20 +1,16 @@
 export const BotonesHeader = () => {
   return (
-    <>
-      <div className="flex flex-row gap-75">
-        <button
-          type="button"
-          className="py-1 px-9 me-2 mb-2 text-sm font-bold text-white bg-myYellow rounded-full border border-myYellow hover:bg-myGreen hover:text-myYellow transition-colors duration-300 cursor-pointer whitespace-pre-line">
-          {`Mi Total:
-          1000 €`}
-        </button>
-        <button
-          type="button"
-          className="py-1 px-5 me-2 mb-2 text-sm font-bold text-white bg-myYellow rounded-full border border-myYellow hover:bg-myGreen hover:text-myYellow transition-colors duration-300 cursor-pointer whitespace-pre-line">
-          {`Límite diario:
-           30 €`}
-        </button>
-      </div>
-    </>
+    <div className="flex flex-col md:flex-row gap-2 md:gap-55">
+      <button
+        type="button"
+        className="py-1 px-8 text-sm font-bold text-white bg-myYellow rounded-full border border-myYellow hover:bg-myGreen hover:text-myYellow transition-colors duration-300 cursor-pointer whitespace-pre-line">
+        {`Mi Total:\n1000 €`}
+      </button>
+      <button
+        type="button"
+        className="py-1 px-4 text-sm font-bold text-white bg-myYellow rounded-full border border-myYellow hover:bg-myGreen hover:text-myYellow transition-colors duration-300 cursor-pointer whitespace-pre-line">
+        {`Límite diario:\n30 €`}
+      </button>
+    </div>
   );
 };
