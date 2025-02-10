89361bd9a2e9148c108449c0dd836598755ca1b4
Orealba Soriano <orealbas@gmail.com>
2025-02-10 13:12:58 +0100

    creating the body

---
+++ src/Components/Body.tsx
@@ -0,0 +1,13 @@
+import { BotonProxGastos } from './Botones/BotonProxGastos';
+
+export const Body = () => {
+  return (
+    <>
+      <div className="bg-myGray/50 rounded-2xl px-8 sm:px-8 md:px-20 lg:px-30 py-4 sm:py-6 md:py-8 lg:py-10 mt-2 sm:mt-3 md:mt-4 lg:mt-5">
+        <div>
+          <BotonProxGastos></BotonProxGastos>
+        </div>
+      </div>
+    </>
+  );
+};
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,10 +1,11 @@
-
 import { Header } from '../Components/Header';
+import { Body } from '../Components/Body';
 export const Home = () => {
   return (
     <>
       <div>
         <Header></Header>
+        <Body></Body>
       </div>
     </>
   );
