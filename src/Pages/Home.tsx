48a7c1654e12939fc0b2f7a11f9ac2a9bf142f62
Orealba Soriano <orealbas@gmail.com>
2025-02-10 13:17:20 +0100

    created the body

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,11 +1,10 @@
+
 import { Header } from '../Components/Header';
-import { Body } from '../Components/Body';
 export const Home = () => {
   return (
     <>
       <div>
         <Header></Header>
-        <Body></Body>
       </div>
     </>
   );
